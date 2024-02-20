d0e1635f2c9c1c206df90eef828e84b60e052fa0
4rdnac <candraahmaddani04@gmail.com>
2024-02-20 07:57:01 +0700

    P2_Percobaan1_08

---
+++ P2/Buku08.java
@@ -0,0 +1,26 @@
+package P2;
+
+public class Buku08 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilkanInformasi() {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Jumlah Halamag : " + halaman);
+        System.out.println("Sisa stok : " + stok);
+        System.out.println("Harga : Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
