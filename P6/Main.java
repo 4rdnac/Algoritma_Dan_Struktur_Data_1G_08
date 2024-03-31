9d27e40e5093d4ac3e28a1f4aa25a27a05a240bf
4rdnac <candraahmaddani04@gmail.com>
2024-03-31 12:42:51 +0700

    P6_Percobaan3_08

--- P6/Main.java
+++ P6/Main.java
@@ -27,5 +27,10 @@ public class Main {
         System.out.println("Data mahasiswa stelah sorting asc berdasarkan ipk");
         list.selectionSort();
         list.tampil();
+
+        System.out.println();
+        System.out.println("Data mahasiswa setelah sorting asc berdasarkan ipk");
+        list.insertionSort();
+        list.tampil();
     }
 }
