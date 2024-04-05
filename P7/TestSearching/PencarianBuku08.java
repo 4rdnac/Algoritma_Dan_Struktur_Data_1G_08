645dfa0b5a9964573910f9d16fb75921c9e02c83
4rdnac <candraahmaddani04@gmail.com>
2024-04-05 15:29:31 +0700

    P7_Percobaan1_08

---
+++ P7/TestSearching/PencarianBuku08.java
@@ -0,0 +1,40 @@
+package P7.TestSearching;
+
+public class PencarianBuku08 {
+    Buku08 listBk[] = new Buku08[5];
+    int idx;
+
+    void tambah(Buku08 m) {
+        if (idx < listBk.length) {
+            listBk[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }
+
+    void tampil() {
+        for (Buku08 m : listBk) {
+            m.TampilDataBuku08();
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int i = 0; i < listBk.length; i++) {
+            if (listBk[i].kodeBuku == cari) {
+                posisi = i;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void TampilPosisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("data : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + " tidak ditemukan");
+        }
+    }
+}
