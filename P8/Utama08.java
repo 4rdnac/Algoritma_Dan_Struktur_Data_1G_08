bd26e6c270711a017126d9a353a95b122401bea1
4rdnac <candraahmaddani04@gmail.com>
2024-04-28 00:15:54 +0700

    P8_Percobaan1_08

---
+++ P8/Utama08.java
@@ -0,0 +1,46 @@
+package P8;
+
+import java.util.Scanner;
+
+public class Utama08 {
+    public static void main(String[] args) {
+        Gudang08 gudang = new Gudang08(7);
+        Scanner sc08 = new Scanner(System.in);
+        boolean isRun = true;
+        while (isRun) {
+            System.out.println("\nMenu :");
+            System.out.println("1. Tambah barang");
+            System.out.println("2. Ambil barang");
+            System.out.println("3. Tampilkan tumpukan barang");
+            System.out.println("4. keluar");
+            System.out.print("Pilih Operasi: ");
+            int pilihan = sc08.nextInt();
+            sc08.nextLine();
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan kode barang: ");
+                    int kode = sc08.nextInt();
+                    sc08.nextLine();
+                    System.out.print("Masukkan nama barang: ");
+                    String nama = sc08.nextLine();
+                    System.out.print("Masukkan kategori barang: ");
+                    String kategori = sc08.nextLine();
+                    Barang08 barangBaru = new Barang08(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+                case 2:
+                    gudang.ambilBarang();
+                    break;
+                case 3:
+                    gudang.tampilBarang();
+                    break;
+                case 4:
+                    isRun = false;
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. silahkan coba lagi.");
+                    break;
+            }
+        }
+    }
+}
