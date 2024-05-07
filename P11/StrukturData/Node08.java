f925a332c2a8b69b4dcc615515a334d85d8f773a
4rdnac <candraahmaddani04@gmail.com>
2024-05-07 14:08:29 +0700

    P11_Percobaan1_08 revisi

--- P11/StrukturData/Node08.java
+++ P11/StrukturData/Node08.java
@@ -10,5 +10,6 @@ public class Node08 {
     }
 
     Node08() {
+        
     }
 }
--- P11/StrukturData/SingleLinkedList08.java
+++ P11/StrukturData/SingleLinkedList08.java
@@ -60,25 +60,18 @@ public class SingleLinkedList08 {
     }
 
     void insertAt(int index, int input) {
-        Node08 ndInput = new Node08();
         if (index < 0) {
-            System.out.println("Perbaiki logikanya! kalau indeksnya -1 bagaimana???");
-            return;
+            System.out.println("Perbaiki logikanya! Kalau indeksnya -1 bagaimana?");
         } else if (index == 0) {
             addFirst(input);
         } else {
             Node08 temp = head;
-            for (int i = 0; i < index - 1; i++) {
-                if (temp == null) {
-                    System.out.println("Indeks melebihi ukuran linked list");
-                    return;
-                }
+            for (int i = 0; i < index; i++) {
                 temp = temp.next;
             }
-            ndInput.next = temp.next;
-            temp.next = ndInput;
-            if (ndInput.next == null) {
-                tail = ndInput;
+            temp.next = new Node08(input, temp.next);
+            if (temp.next.next == null) {
+                tail = temp.next;
             }
         }
     }
