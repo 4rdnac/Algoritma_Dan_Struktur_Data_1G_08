2d730b3f5f1b537bbedf2f680a9080470ebcf2c8
4rdnac <candraahmaddani04@gmail.com>
2024-05-08 01:46:10 +0700

    P11_Percobaan2_08

--- P11/StrukturData/SingleLinkedList08.java
+++ P11/StrukturData/SingleLinkedList08.java
@@ -66,12 +66,98 @@ public class SingleLinkedList08 {
             addFirst(input);
         } else {
             Node08 temp = head;
-            for (int i = 0; i < index -1; i++) {
+            for (int i = 0; i < index - 1; i++) {
                 temp = temp.next;
             }
             temp.next = new Node08(input, temp.next);
             if (temp.next.next == null) {
                 tail = temp.next;
+            }
+        }
+    }
+
+    int getData(int index) {
+        Node08 temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        return temp.next.data;
+    }
+
+    int indexOf(int key) {
+        Node08 temp = head;
+        int index = 0;
+        while (temp != null && temp.data != key) {
+            temp = temp.next;
+            index++;
+        }
+        if (temp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list ini masih kosong"
+                    + "tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list ini masih kosong"
+                    + "tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node08 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp.next;
+        }
+    }
+
+    void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("Linked list ini masih kosong"
+                    + "tidak dapat dihapus");
+        } else {
+            Node08 temp = head;
+            while (temp != null) {
+                if (temp.data == key && temp == head) {
+                    removeFirst();
+                    break;
+                } else if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node08 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
             }
         }
     }
