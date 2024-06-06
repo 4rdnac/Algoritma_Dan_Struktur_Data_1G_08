05ebd3c2bf14dc8af623a068ddda6d5b5ad1bf3d
4rdnac <candraahmaddani04@gmail.com>
2024-06-06 14:55:51 +0700

    P15_Percobaan2_08

--- P15/GraphMain08.java
+++ P15/GraphMain08.java
@@ -27,6 +27,16 @@ public class GraphMain08 {
         tujuan = sc08.nextInt();
         gedung.isConnected(asal, tujuan);
         sc08.close();
-
+        GraphMatriks08 gdg = new GraphMatriks08(4);
+        gdg.makeEdge(0, 1, 50);
+        gdg.makeEdge(1, 0, 60);
+        gdg.makeEdge(1, 2, 70);
+        gdg.makeEdge(2, 1, 80);
+        gdg.makeEdge(2, 3, 40);
+        gdg.makeEdge(3, 0, 90);
+        gdg.printGraph();
+        System.out.println("Hasil setelah penghapusan edge");
+        gdg.removeEdge(2, 1);
+        gdg.printGraph();
     }
 }
